ace2b12e0619eefd6d32afff99f3d1c2ad4a36c7
Angelini <lllouis@yahoo.com>
2017-11-11 18:29:25 +0000

    Getting token back

--- controllers/auth_controller.js
+++ controllers/auth_controller.js
@@ -1,15 +1,35 @@
 'use strict'
 
 var models = require('../models')
+var jwt = require("jwt-simple");
+var config = require("../config/strategy-config")
 
 module.exports.controller = function( app ) {
 	app.post('/login', function( req, res ) {
+
+		if (req.body.email) {
+		        var email = req.body.email;
+		        models.User.findOne({
+		        			where: { id: req.body.id }
+		        		}).then( user => {
+		        			if (user) {
+					            var payload = {
+					                id: user.id
+					            };
+					            var token = jwt.encode(payload, config.jwtSecret);
+					            res.json({
+					                token: token
+					            });
+					        } else {
+					            res.sendStatus(401);
+					        }
+		        		})
+		        
+		    } else {
+		        res.sendStatus(401);
+		    }
 		
-		models.User.findOne({
-			where: { id: req.body.id }
-		}).then( user => {
-			res.json( user )
-		})
+		
 		
 	})
 }
