b4a9bb59e306a02fa399742a7ebcec6ae8b81bad
Angelini <lllouis@yahoo.com>
2017-11-11 18:49:25 +0000

    Register route created

--- controllers/auth_controller.js
+++ controllers/auth_controller.js
@@ -8,28 +8,41 @@ module.exports.controller = function( app ) {
 	app.post('/login', function( req, res ) {
 
 		if (req.body.email) {
-		        var email = req.body.email;
-		        models.User.findOne({
-		        			where: { id: req.body.id }
-		        		}).then( user => {
-		        			if (user) {
-					            var payload = {
-					                id: user.id
-					            };
-					            var token = jwt.encode(payload, config.jwtSecret);
-					            res.json({
-					                token: token
-					            });
-					        } else {
-					            res.sendStatus(401);
-					        }
-		        		})
+	        var email = req.body.email;
+	        models.User.findOne({
+	        			where: { id: req.body.id }
+	        		}).then( user => {
+	        			if (user) {
+				            var payload = {
+				                id: user.id
+				            };
+				            var token = jwt.encode(payload, config.jwtSecret);
+				            res.json({
+				                token: token
+				            });
+				        } else {
+				            res.sendStatus(401);
+				        }
+	        		})
 		        
-		    } else {
-		        res.sendStatus(401);
-		    }
+	    } else {
+	        res.sendStatus(401);
+	    }
+
 		
-		
-		
+	})
+
+	app.post('/register', function( req, res ) {
+		if ( req.body.email && req.body.password ) {
+			models.User.findOrCreate({
+				where: { email: req.body.email }
+			}).spread( ( user, created ) => {
+				console.log(user.get({
+				    plain: true
+				}))
+				console.log(created)
+				res.json( {email: user.email, new: created} )
+			})
+		}
 	})
 }
