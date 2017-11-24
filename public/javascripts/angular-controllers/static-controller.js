eefcde30b8369dbf2d9e624019a7daf70933e357
Loubot <lllouis@yahoo.com>
2017-11-24 19:27:41 +0000

    Display photos in index

--- public/javascripts/angular-controllers/static-controller.js
+++ public/javascripts/angular-controllers/static-controller.js
@@ -12,6 +12,25 @@ angular.module('portfolio').controller( 'staticController', [
 
 		$scope.$mdMedia = $mdMedia
 
+		$scope.img_urls = new Array()
+		/* Get all objects in port-practise bucket */
+		$http({
+			method: 'GET',
+			url: window.location.origin + '/s3-list-all',
+			headers: {
+				"Authorization": "Bearer " + window.localStorage.getItem( 'token' )
+			}
+		}).then( function successCallBack( res ) {
+			console.log( res.data )
+			for ( let img of res.data.Contents ){
+				$scope.img_urls.push( "https://s3-eu-west-1.amazonaws.com/als-portfolio/" + img.Key )
+
+			}
+			console.log($scope.img_urls)
+		}), function error( err ) {
+			console.log( err )
+		}
+
 		$scope.openLeftMenu = function() {
 			$mdSidenav( 'left' ).toggle()
 		}
