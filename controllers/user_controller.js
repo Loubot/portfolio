beb130961afc79bf889029793799ede25e626fde
Loubot <lllouis@yahoo.com>
2017-11-24 19:28:52 +0000

    Reduce timeout

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -53,7 +53,7 @@ module.exports.controller = function( app, strategy ) {
 		    Bucket: config.Bucket,
 		    Key:req.query.Key,
 		    ContentType:req.query.type,
-		    Expires: 600
+		    Expires: 60
 		};
 
 		if ( req.query.request_type === 'putObject' ){
