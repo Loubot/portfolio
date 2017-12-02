c558d18ff7b619e46b3e1be7f631befd055935d2
Angelini <lllouis@yahoo.com>
2017-12-02 17:34:39 +0000

    Update code with bluebird

--- controllers/images_controller.js
+++ controllers/images_controller.js
@@ -1,16 +1,19 @@
 'use strict'
 
+var Promise = require('bluebird')
 var AWS = require('aws-sdk');
-var fs = require( 'fs')
+var fs = Promise.promisifyAll( require( 'fs') )
 AWS.config.update({region: 'eu-west-1'});
 var config = require("../config/s3-config")
 // console.log( config )
 AWS.config.update(config);
-var s3 = new AWS.S3();
+var s3 = 	Promise.promisifyAll( new AWS.S3() )
 var winston = require('winston')
 
-var mkdirp = require('mkdirp')
-var Jimp = require('jimp')
+var mkdirp = Promise.promisifyAll( require('mkdirp') )
+var Jimp = Promise.promisifyAll( require('jimp') )
+
+
 
 module.exports.controller = function( app, strategy ) {
 	/* /s3-list-all params: { } */
@@ -67,9 +70,15 @@ module.exports.controller = function( app, strategy ) {
 		});
 	})
 
+	/* /photo params: { file_name } */
+	/* Expects filename of recently uploaded photo. Creates thumbsize version of file and pushes this thumb
+		to s3 bucket. 
+	*/
+
 	app.post('/photo', strategy.authenticate(), function( req, res ) {
 		console.log( '/image images_controller' )
 		winston.debug( req.body )
+		
 		mkdirp( 'tmp/images', function( err ) {
 			if ( err ) {
 				res.status( 422 ).json( "Can't create directory" )
@@ -85,52 +94,37 @@ module.exports.controller = function( app, strategy ) {
 					
 				}).on( 'close', function() {
 					out.end()
-					
-					Jimp.read( ( "./tmp/images/" + req.body.file_name ), function( err, img ) {
-						if ( err ) {
-							winston.debug( 'big err')
-							winston.debug( err )
-							res.status( 404 ).json( "Faied to read file from local storage" )
-							return false
-						} else {
-							
-							winston.debug( "Found file" )
-							img.scaleToFit(256, 256)            // resize
-							     .quality(60)                 // set JPEG quality
-							     .write("./tmp/images/thumb_" + req.body.file_name); // save
-							winston.debug( 'finished')
-							console.log('5')
-							fs.readFile( ( "./tmp/images/thumb_" + req.body.file_name ), function( err, file ) {
-								if ( err ) {
-									winston.debug(err)
-								} else {
-									
-									var params = {
-									  	Body: file, 
-									  	Bucket: config.Bucket, 
-									 	Key: "thumb_" + req.body.file_name,
-									}
 
-									s3.putObject( params, function( err, data ) {
-										if ( err ) {
-											res.status( 422 ).json( err )
-										} else {
-											
-											res.json({ 
-												message: "File uploaded and thumb created",
-												s3_response: data
-											})
-										}
-									})
-								}
-							})
-							
-						}
-					})
-				})
-			}
-		})
-	})
+					Jimp.readAsync( ( "./tmp/images/" + req.body.file_name ) ).then( function( img ) {
+						winston.debug( "Found file" )
+						img.scaleToFit(256, 256)            // resize
+						     .quality(60)                 // set JPEG quality
+						     .write("./tmp/images/thumb_" + req.body.file_name); // save
+						winston.debug( 'finished')
+						return fs.readFileAsync( ( "./tmp/images/thumb_" + req.body.file_name ) )
+					}).then( function( file ) {
+						var params = {
+						  	Body: file, 
+						  	Bucket: config.Bucket, 
+						 	Key: "thumb_" + req.body.file_name,
+						} /*End of params*/
+
+						return s3.putObjectAsync( params )
+					}).then( function ( data ) {
+						res.json({ 
+							message: "File uploaded and thumb created",
+							s3_response: data
+						})
+					}).catch(function (err) {
+			            winston.debug( "Error in Jimp.read or fs.readFileAsync or s3.putObject" )
+						winston.debug( err )
+
+						res.status( 500 ).json( err )
+			        })
+				}) /*end on('close'*/
+			} /* end else*/
+		}) /* End mkdirp */
+	}) /*End app.post( 'photo' ) */
 
 	/* /image params: { } */
 	/* Download an image from the bucket alter it and push it pack with a different name*/
