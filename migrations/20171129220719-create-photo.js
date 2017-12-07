ace97bfa963015026f9b02db548c1dace1792730
Loubot <lllouis@yahoo.com>
2017-12-07 22:25:18 +0000

    Change migrations

--- migrations/20171129220719-create-photo.js
+++ migrations/20171129220719-create-photo.js
@@ -17,6 +17,9 @@ module.exports = {
       UserId: {
         type: Sequelize.INTEGER
       },
+      CategoryId: {
+        type: Sequelize.INTEGER
+      },
       createdAt: {
         allowNull: false,
         type: Sequelize.DATE
