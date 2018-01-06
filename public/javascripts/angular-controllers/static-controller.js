07d930ff1c611c60e9c6797819ae314d33e866ca
Angelini <lllouis@yahoo.com>
2018-01-06 11:47:53 +0000

    Fix index page

--- public/javascripts/angular-controllers/static-controller.js
+++ public/javascripts/angular-controllers/static-controller.js
@@ -5,7 +5,8 @@ angular.module('portfolio').controller( 'staticController', [
 	"$http",
 	"$state",
 	"Alertify",
-	function( $scope, $http, $state, Alertify ) {
+	'imageClass',
+	function( $scope, $http, $state, Alertify, imageClass ) {
 		console.log('staticController')
 
 		$scope.img_urls = new Array()
@@ -15,7 +16,36 @@ angular.module('portfolio').controller( 'staticController', [
 			url: window.location.origin + '/api/photos'
 		}).then( function success( res ) {
 			console.log( res )
-			$scope.images = res.data
+			var tempImages = []
+			var imageSize = function( i, total ) {
+				
+				if( i === total ) {
+					
+					// console.log( tempImages )
+					$scope.images = tempImages 
+					console.log( $scope.images )
+					if ( $scope.images.length) $scope.$apply()
+					return
+					// $scope.images = tempImages
+				}
+				var image = new Image()
+				var img = res.data[ i ]
+				image.src = img.thumbUrl
+				image.onload = function() {
+					img.width = this.width
+					img.height = this.height
+
+					img.class = imageClass( this.height, this.width )
+					
+					tempImages.push( img )
+					// console.log( tempImages )
+					i++
+					imageSize( i, total )
+				}
+				
+			}
+
+			imageSize( 0, res.data.length )
 		}), function error( err ) {
 			console.log( err )
 		}
