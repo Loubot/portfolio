6f02fbdec37b0802da8374f85278ee5ff45c1339
Angelini <lllouis@yahoo.com>
2018-02-01 02:27:11 +0000

    close dialog

--- public/javascripts/angular-controllers/static-controller.js
+++ public/javascripts/angular-controllers/static-controller.js
@@ -41,6 +41,19 @@ angular.module('portfolio').controller( 'staticController', [
 						
 					}
 
+					$scope.fb_share = function( url ) {
+						console.log( url )
+						FB.ui({
+						  method: 'feed',
+						  link: "https://als-portfolio.herokuapp.com",
+						  source: url,
+						  picture: url,
+						  caption: 'An example caption'
+						}, function( res ) {
+							console.log( res )
+						})
+					}
+
 					$scope.close_dialog = function() {
 						$mdDialog.hide()
 					}
