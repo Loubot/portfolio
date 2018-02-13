e8853048d16bfc66182b3362ff093517b6e813f9
Angelini <Lllouis@yahoo.com>
2018-02-13 10:48:59 +0000

    Send message from postman

--- controllers/static_controller.js
+++ controllers/static_controller.js
@@ -6,4 +6,34 @@ module.exports.controller = function ( app, strategy ) {
 	app.get('/', function( req, res ) {
 		res.render('index')
 	})
+
+	app.post( '/api/mail', strategy.authenticate(), function( req, res ) {
+		winston.debug( 'mailer ')
+		winston.debug( req.body )
+		var nodemailer = require('nodemailer')
+		var transporter = nodemailer.createTransport({
+		  service: 'gmail',
+		  auth: {
+		    user: 'louisangelini@gmail.com',
+		    pass: process.env.gpass
+		  }
+		});
+
+		var mailOptions = {
+		  from: req.body.email,
+		  to: 'lllouis@yahoo.com',
+		  subject: 'Sending Email using Node.js',
+		  text: req.body.message
+		};
+
+		transporter.sendMail(mailOptions, function(error, info){
+		  if (error) {
+		    winston.debug(error);
+		    res.json( error )
+		  } else {
+		    winston.debug('Email sent: ' + info.response);
+		    res.json( info )
+		  }
+		});
+	})
 }
--- public/javascripts/angular-controllers/contact-controller.js
+++ public/javascripts/angular-controllers/contact-controller.js
@@ -4,5 +4,10 @@ angular.module( 'portfolio' ).controller( 'contactController', [
 	'$scope',
 	function( $scope ) {
 		console.log( 'contactController' )
+
+
+		$scope.send_message = function() {
+			console.log( $scope.form )
+		}
 	}
 ])
