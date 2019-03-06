0f9052a8e26b6238822925d4178ada63888c3ac7
Angelini <lllouis@yahoo.com>
2019-03-06 16:27:04 +0000

    Deleting subcategory and photos

--- models/subcategory.js
+++ models/subcategory.js
@@ -1,4 +1,6 @@
 var winston = require( 'winston' )
+let models = require( '../models' )
+
 'use strict';
 module.exports = (sequelize, DataTypes) => {
     var subCategory = sequelize.define('subCategory', {
@@ -18,7 +20,35 @@ module.exports = (sequelize, DataTypes) => {
     subCategory.hook( 'beforeDestroy', function( category, options, fn ) {
         winston.debug( 'subCategory beforeDestroy hook' )
         winston.debug( category.dataValues )
-        fn( null, category )
+        winston.debug( 'Try to find all photos with subCategoryId' )
+        sequelize.models.Photo.findAll({
+            where: { subCategoryId: category.dataValues.id }
+        }).then( photos => {
+            winston.debug( 'Found all photos with subCategoryId' )
+            let photos_arr = []
+            winston.debug( photos )
+            photos.forEach( photo => {
+                photos_arr.push( photo.dataValues.id )
+            })
+            winston.debug( photos_arr )
+            sequelize.models.Photo.destroy({
+                where: { id: photos_arr }
+            }).then( deleted => {
+                winston.debug( 'Deleted all photos' )
+                winston.debug( deleted )
+                fn( null, deleted )
+            }).catch( err => {
+                winston.debug( 'Failed to delete photos' )
+                winston.debug( err )
+                fn( err )
+            })
+            
+        }).catch( err => {
+            winston.debug( 'Failed to find photos' )
+            winston.debug( err )
+            fn( err )
+        })
+        
     })
     return subCategory;
 };
