8fa27e38bdcc7d0f5694317a30c56f1dd75cb448
Angelini <lllouis@yahoo.com>
2019-04-19 18:33:29 +0100

    Try key checks in model

--- migrations/20170221140042-create-sub-category.js
+++ migrations/20170221140042-create-sub-category.js
@@ -22,6 +22,7 @@ module.exports = {
     });
   },
   down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('subCategories');
+    return queryInterface.sequelize.query('SET FOREIGN_KEY_CHECKS = 0').then(
+     queryInterface.dropTable('subCategories'))
   }
 };
